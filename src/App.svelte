12810de72c0afdb75e4a9d204b7c95046c77e290
Franxois <franxoois@gmail.com>
2020-11-04 21:57:54 +0100

    feat(distance): show location and distance from home

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,111 @@
-<script lang='typescript'>
-	import {onMount} from 'svelte';
-	let count: number = 0;
-	onMount(() => {
-	  const interval = setInterval(() => count++, 1000);
-	  return () => {
-		clearInterval(interval);
-	  };
-	});
-  </script>
-  
-  <style>
-	:global(body) {
-	  margin: 0;
-	  font-family: Arial, Helvetica, sans-serif;
-	}
-	.App {
-	  text-align: center;
-	}
-	.App code {
-	  background: #0002;
-	  padding: 4px 8px;
-	  border-radius: 4px;
-	}
-	.App p {
-	  margin: 0.4rem;
-	}
-  
-	.App-header {
-	  background-color: #f9f6f6;
-	  color: #333;
-	  min-height: 100vh;
-	  display: flex;
-	  flex-direction: column;
-	  align-items: center;
-	  justify-content: center;
-	  font-size: calc(10px + 2vmin);
-	}
-	.App-link {
-	  color: #ff3e00;
-	}
-	.App-logo {
-	  height: 36vmin;
-	  pointer-events: none;
-	  margin-bottom: 3rem;
-	  animation: App-logo-spin infinite 1.6s ease-in-out alternate;
-	}
-	@keyframes App-logo-spin {
-	  from {
-		transform: scale(1);
-	  }
-	  to {
-		transform: scale(1.06);
-	  }
-	}
-  </style>
-  
-  <div class="App">
-	<header class="App-header">
-	  <img src="/logo.svg" class="App-logo" alt="logo" />
-	  <p>Edit <code>src/App.svelte</code> and save to reload.</p>
-	  <p>Page has been open for <code>{count}</code> seconds.</p>
-	  <p>
-		<a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
-		  Learn Svelte
-		</a>
-	  </p>
-	</header>
-  </div>
-  
+<script lang="ts">
+  import { calculateDistance } from "./distance";
+
+  let latitude: number;
+  let longitude: number;
+  let accuracy: number;
+  let lastFix: Date;
+
+  const KEY_LOCATION = "confinement:home";
+
+  navigator.geolocation.watchPosition(function (position) {
+    console.log(position);
+    // Destructuring without affectation
+    ({ latitude, longitude, accuracy } = position.coords);
+    lastFix = new Date();
+    lastFix.setTime(position.timestamp);
+  });
+
+  let reference = {} as { latitude: number; longitude: number };
+  const saved = window.localStorage.getItem(KEY_LOCATION);
+  if (saved !== null) {
+    reference = JSON.parse(saved);
+  }
+
+  const saveLocation = () => {
+    reference = { latitude, longitude };
+    window.localStorage.setItem(KEY_LOCATION, JSON.stringify(reference));
+  };
+
+  $: precision =
+    accuracy !== undefined ? Math.round(accuracy) + "m" : undefined;
+
+  $: humanDate = lastFix !== undefined ? lastFix.toLocaleString() : undefined;
+
+  let distance: string;
+  let link2homeGmap: string;
+
+  $: if (reference.latitude && reference.longitude && latitude && longitude) {
+    distance =
+      Math.round(
+        calculateDistance(
+          reference.latitude,
+          reference.longitude,
+          latitude,
+          longitude
+        ) * 1000
+      ) + "m";
+    link2homeGmap =
+      "https://www.google.com/maps/search/?api=1&query=" +
+      reference.latitude +
+      "," +
+      reference.longitude;
+  }
+</script>
+
+<style>
+  :global(body) {
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  .App {
+    text-align: center;
+  }
+  .App code {
+    background: #0002;
+    padding: 4px 8px;
+    border-radius: 4px;
+  }
+  .App p {
+    margin: 0.4rem;
+  }
+
+  .App-header {
+    background-color: #f9f6f6;
+    color: #333;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+  }
+
+  .App-logo {
+    height: 36vmin;
+    pointer-events: none;
+    margin-bottom: 3rem;
+    animation: App-logo-spin infinite 1.6s ease-in-out alternate;
+  }
+  @keyframes App-logo-spin {
+    from {
+      transform: scale(1);
+    }
+    to {
+      transform: scale(1.06);
+    }
+  }
+</style>
+
+<div class="App">
+  <header class="App-header">
+    <img src="compass-svgrepo-com.svg" class="App-logo" alt="logo" />
+    <p>Position : {latitude},{longitude}</p>
+    <p>Précision : {precision}</p>
+    <p>Date : {humanDate}</p>
+    {#if distance}
+      <p>Distance : {distance} from <a href={link2homeGmap}>🏠</a></p>
+    {/if}
+    <button on:click={saveLocation}>save position</button>
+  </header>
+</div>
---
+++ src/distance.ts
@@ -0,0 +1,22 @@
+export function calculateDistance(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+) {
+  var R = 6371; // km
+  var dLat = toRad(lat2 - lat1);
+  var dLon = toRad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) *
+      Math.cos(toRad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
+}
+const toRad = function (x: number) {
+  return (x * Math.PI) / 180;
+};
