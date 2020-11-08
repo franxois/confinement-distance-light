9785eb8153ae99986c416acd1d9ad930ba1cd152
Franxois <franxoois@gmail.com>
2020-11-08 23:02:01 +0100

    refactor(template): replace Jest by @web/test-runner, copied from new version of template

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,8 +1,11 @@
 import { render } from "@testing-library/svelte";
+import { expect } from "chai";
 import App from "./App.svelte";
 
-test("renders learn svelte link", () => {
-  const { getByText } = render(App);
-  const linkElement: HTMLElement = getByText(/learn svelte/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App>", () => {
+  it("renders main page", () => {
+    const { getByText } = render(App);
+    const labelElement = getByText(/Position/);
+    expect(document.body.contains(labelElement));
+  });
 });
