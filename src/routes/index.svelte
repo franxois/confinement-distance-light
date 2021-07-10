1c96a569fca351e23eab18947162be1089acf6a5
Franxois <franxoois@gmail.com>
2021-07-10 15:12:04 +0200

    build(sveltekit): migrate to svelkit

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+	// Disable ssr because <Position/> uses navigator.geolocation, a web API
+	export const ssr = false;
+</script>
+
+<script lang="ts">
+	import Position from '$lib/Position/index.svelte';
+</script>
+
+<svelte:head>
+	<title>Confinement distance light</title>
+</svelte:head>
+
+<section>
+	<Position />
+</section>
+
+<style lang="scss">
+	section {
+		text-align: center;
+	}
+</style>
